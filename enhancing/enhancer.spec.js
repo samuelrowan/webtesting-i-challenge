3f33cd6924de7b8c60feea76eb931a824efe5211
Samuel Rowan <samuel@samuelrowan.com>
2020-05-13 18:08:23 -0500

    Update enhancer.spec.js

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,7 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe("enhancer unit tests", () => {
+    it("repairs", () => {
+        expect(enhancer.repair({name:"item", durability:20}).toBe({name:"item", durability:100}))
+    })
+})
